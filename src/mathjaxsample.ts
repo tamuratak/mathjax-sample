04566f610b3158bc965dd11c7ddc054fa8d25827
Takashi Tamura <tamuratak@users.noreply.github.com>
2021-11-05 21:00:01 +0900

    edit

--- src/mathjaxsample.ts
+++ src/mathjaxsample.ts
@@ -1,10 +1,11 @@
-import { mathjax } from 'mathjax-full/js/mathjax';
+import type {SvgOption, TexOption} from 'mathjax-full'
+import { mathjax } from 'mathjax-full/js/mathjax.js';
 import { TeX } from 'mathjax-full/js/input/tex.js';
 import { SVG } from 'mathjax-full/js/output/svg.js';
 import { liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor.js';
 import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html.js';
 import { LiteElement } from 'mathjax-full/js/adaptors/lite/Element';
-// import { MathDocument } from 'mathjax-full/js/core/MathDocument';
+import { MathDocument } from 'mathjax-full/js/core/MathDocument';
 import { LiteDocument } from 'mathjax-full/js/adaptors/lite/Document';
 import { LiteText } from 'mathjax-full/js/adaptors/lite/Text';
 
@@ -29,9 +30,11 @@ RegisterHTMLHandler(adaptor);
 //
 //  Create input and output jax and a document using them on the content from the HTML file
 //
-const tex = new TeX<LiteElement, LiteText, LiteDocument>({packages: ['base', 'ams']});
-const svg = new SVG<LiteElement, LiteText, LiteDocument>({fontCache: 'local'});
-const html = mathjax.document('', {InputJax: tex, OutputJax: svg})
+const texOption: TexOption = {packages: ['base', 'ams']}
+const tex = new TeX<LiteElement, LiteText, LiteDocument>(texOption);
+const svgOption: SvgOption = {fontCache: 'local'}
+const svg = new SVG<LiteElement, LiteText, LiteDocument>(svgOption);
+const html = mathjax.document('', {InputJax: tex, OutputJax: svg}) as MathDocument<LiteElement, LiteText, LiteDocument>;
 
 //
 //  Typeset the math from the command line
@@ -41,7 +44,7 @@ const node = html.convert('a+b', {
     em: 16,
     ex: 8,
     containerWidth: 80*16
-})
+}) as LiteElement
 
 //
 //  If the --css option was specified, output the CSS,
--- types/mathjax/index.d.ts
+++ types/mathjax/index.d.ts
@@ -1,89 +1,4 @@
-import { mathjax } from 'mathjax-full/js/mathjax';
-import { TeX } from 'mathjax-full/js/input/tex.js';
-import { SVG } from 'mathjax-full/js/output/svg.js';
-import { liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor.js';
-import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html.js';
-import { LiteElement } from 'mathjax-full/js/adaptors/lite/Element';
-import { MathDocument } from 'mathjax-full/js/core/MathDocument';
-import { LiteDocument } from 'mathjax-full/js/adaptors/lite/Document';
-import { LiteText } from 'mathjax-full/js/adaptors/lite/Text';
-
-export const init: (arg: InitArg) => Promise<MathJaxApplication>
-
-export type TypesetArg = {
-    width?: number,
-    equationNumbers?: string,
-    math: string,
-    format: string,
-    svgNode: boolean,
-    state?: {
-        AMS: {
-            labels: { [k: string]: string },
-            IDs: { [k: string]: string },
-            startNumber: number
-        }
-    }
-}
-
-export type TypesetReturnType = {
-    svgNode: {
-        getAttribute: (name: string) => string,
-        setAttribute: (name: string, value: string) => void,
-        outerHTML: string
-    }
-}
-
-type SupportedExtension =
-    'action' |
-    'ams' |
-    'amscd' |
-    'autoload' |
-    'base' |
-    'bbox' |
-    'boldsymbol' |
-    'braket' |
-    'bussproofs' |
-    'cancel' |
-    'cases' |
-    'centernot' |
-    'color' |
-    'colortbl' |
-    'colorv2' |
-    'configmacros' |
-    'empheq' |
-    'enclose' |
-    'extpfeil' |
-    'gensymb' |
-    'html' |
-    'mathtools' |
-    'mhchem' |
-    'newcommand' |
-    'noerrors' |
-    'noundefined' |
-    'physics' |
-    'require' |
-    'setoptions' |
-    'tagformat' |
-    'textcomp' |
-    'textmacros' |
-    'unicode' |
-    'upgreek' |
-    'verb'
-
-type InitArg = {
-    options: {
-        enableMenu?: boolean,
-        skipHtmlTags?: string[],
-        includeHtmlTags?: { [key: string]: string },
-        ignoreHtmlClass?: string,
-        processHtmlClass?: string,
-        enableAssistiveMml?: boolean
-    },
-    loader: {
-        load: string[],
-        require: typeof require
-    },
-    tex: {
+export type TexOption = {
         packages?: SupportedExtension[],
         inlineMath?: [string, string][],
         displayMath?: [string, string][],
@@ -98,51 +13,18 @@ type InitArg = {
         maxMacros?: number,
         maxBuffer?: number,
         baseURL?: string
-    },
-    svg: {
-        scale?: number,
-        minScale?: number,
-        mtextInheritFont?: boolean,
-        merrorInheritFont?: boolean,
-        mathmlSpacing?: boolean,
-        skipAttributes?: { [attrname: string]: boolean },
-        exFactor?: number,
-        displayAlign?: 'left' | 'center' | 'right',
-        displayIndent?: number,
-        fontCache?: 'local' | 'global' | 'none',
-        internalSpeechTitles?: boolean
-    },
-    startup: {
-        typeset: false
-    }
 }
 
-interface MathJaxApplication {
-    startup: {
-        adaptor: LiteAdaptor
-    }
-
-    tex2svgPromise(
-        tex: string,
-        opts: {
-            display: boolean,
-            em: number,
-            ex: number
-            containerWidth: number
-        }
-    ): Promise<LiteElement>
-
-    svgStylesheet(): LiteElement
-}
-
-interface MathDocumentLite extends MathDocument<LiteElement, LiteText, LiteDocument> {
-    convert(math: string, options?: OptionList): LiteElement;
-}
-
-export declare const mathjax: {
-    version: string,
-    document: (
-        document: string,
-        options: {InputJax: TeX<LiteElement, LiteText, LiteDocument>, OutputJax: SVG<LiteElement, LiteText, LiteDocument>}
-    ) => MathDocumentLite
+export type SvgOption = {
+    scale?: number,
+    minScale?: number,
+    mtextInheritFont?: boolean,
+    merrorInheritFont?: boolean,
+    mathmlSpacing?: boolean,
+    skipAttributes?: { [attrname: string]: boolean },
+    exFactor?: number,
+    displayAlign?: 'left' | 'center' | 'right',
+    displayIndent?: number,
+    fontCache?: 'local' | 'global' | 'none',
+    internalSpeechTitles?: boolean
 }
